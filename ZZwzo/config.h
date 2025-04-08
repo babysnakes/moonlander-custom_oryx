01727d39ee307a77b0f142e47b3b17fadd75ce46
Haim Ashkenazi <haim@babysnakes.org>
2025-04-08 18:58:00 +0300

    Enable caps word without assigning special key

--- ZZwzo/config.h
+++ ZZwzo/config.h
@@ -45,3 +45,7 @@
 
 // Combos only from layer 0 (Might be later replaced with DEFAULT_REF_LAYER)
 #define COMBO_ONLY_FROM_LAYER 0
+
+// CAPS Word - enable without assigning secial key
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
